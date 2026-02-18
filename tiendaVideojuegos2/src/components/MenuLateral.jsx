ff23669c8cf2a3b68df024e66db7b29c5246b979
isaac gonzalez <isa.gonzalezl@duocuc.cl>
2026-02-18 00:35:04 -0300

    rutas

--- tiendaVideojuegos2/src/components/MenuLateral.jsx
+++ tiendaVideojuegos2/src/components/MenuLateral.jsx
@@ -8,7 +8,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import CategoryLink from './CategoryLink';
-import "../css/MenuLateral.css"
+import "../css/MenuLateral.css";
 
 
 
